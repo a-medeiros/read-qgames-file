c6125c365151a7158b81e2716e0fb37c97ec80a2
Ana Lúcia Medeiros <ana.medeiros0097@gmail.com>
2022-06-14 19:52:22 -0300

    reading file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import qgame from './qgames.log';
+
 function App() {
+  const readLogFile = async () => {
+    const res = await fetch(qgame);
+    const reader = res.body.getReader();
+    const result = await reader.read();
+    const decoder = new TextDecoder('utf-8');
+    const text = await decoder.decode(result.value);
+    console.log(text);
+  }
+  readLogFile();
+
   return (
     <div>
       <h3>Hello, world!</h3>
