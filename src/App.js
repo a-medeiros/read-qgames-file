aabf912203dc05fe8471fba04df6d56254d00faa
Ana Lúcia Medeiros <ana.medeiros0097@gmail.com>
2022-06-14 23:14:26 -0300

    Converting file content to array

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
+import { useState } from 'react';
 import qgame from './qgames.log';
+import Papa from 'papaparse';
 
 function App() {
+  const [gameData, setGameData] = useState();
+
   const readLogFile = async () => {
     const res = await fetch(qgame);
     const reader = res.body.getReader();
     const result = await reader.read();
     const decoder = new TextDecoder('utf-8');
     const text = await decoder.decode(result.value);
-    console.log(text);
+
+    Papa.parse(text, {
+      complete: function(results) {
+        console.log('teste', results);
+        setGameData(results);
+      }
+    });
   }
   readLogFile();
-
   return (
     <div>
       <h3>Hello, world!</h3>
+      <ul>
+        <li>Informações gerais de cada partida</li>
+        <li>Causa das mortes por partida</li>
+      </ul>
     </div>
   );
 }
